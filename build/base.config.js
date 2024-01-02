bd058305a63dcdb52bee73f232d8d6dc21722ed7
lints <LinTianSheng@newhope.cn>
2024-01-02 16:18:30 +0800

    优化打包

--- build/base.config.js
+++ build/base.config.js
@@ -3,8 +3,9 @@ import { terser } from "rollup-plugin-terser";
 import commonjs from "@rollup/plugin-commonjs";
 import cleanup from "rollup-plugin-cleanup";
 import clear from "rollup-plugin-clear";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
 
-// 插件
 export const basePlugins = [
   typescript({
     useTsconfigDeclarationDir: true
@@ -15,4 +16,35 @@ export const basePlugins = [
   clear({
     targets: ["dist"] // 项目打包编译生成的目录
   })
+];
+
+export const basePluginsDev = [
+  ...basePlugins,
+  livereload({
+    verbose: false
+  }),
+  serve({
+    // open: true, // 运行时自动打开浏览器
+    verbose: false,
+    historyApiFallback: true,
+    contentBase: ".",
+    host: "localhost",
+    port: 10001,
+    onListening: () => {
+      console.log("Listening->", "http://localhost:10001");
+    }
+  })
+];
+
+export const outputDev = [
+  {
+    file: "dist/index.js",
+    format: "es"
+  },
+  {
+    file: "dist/index.umd.js",
+    format: "umd",
+    exports: "named",
+    name: "$c"
+  }
 ];
--- build/test.config.js
+++ build/test.config.js
@@ -1,36 +1,7 @@
-import { basePlugins } from "./base.config";
-import serve from "rollup-plugin-serve";
-import livereload from "rollup-plugin-livereload";
+import { basePluginsDev, outputDev } from "./base.config";
 
 export default {
   input: "./test/index.ts",
-  output: [
-    {
-      file: "dist/index.js",
-      format: "es"
-    },
-    {
-      file: "dist/index.umd.js",
-      format: "umd",
-      exports: "named",
-      name: "$c"
-    }
-  ],
-  plugins: [
-    ...basePlugins,
-    livereload({
-      verbose: false
-    }),
-    serve({
-      // open: true, // 运行时自动打开浏览器
-      verbose: false,
-      historyApiFallback: true,
-      contentBase: ".",
-      host: "localhost",
-      port: 10001,
-      onListening: () => {
-        console.log("Listening->", "http://localhost:10001");
-      }
-    })
-  ]
+  output: outputDev,
+  plugins: basePluginsDev
 };
